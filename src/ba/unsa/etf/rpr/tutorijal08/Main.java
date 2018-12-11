e4af6d21f767b11083931a100cb8d077e6e20c48
asuljagic <43003728+asuljagic@users.noreply.github.com>
2018-12-11 22:09:56 +0100

    Odradjen GUI za oba prozora

--- src/ba/unsa/etf/rpr/tutorijal08/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal08/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
         primaryStage.setTitle("Pretraga datoteka");
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.show();
